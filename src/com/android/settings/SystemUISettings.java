8988ccf3b1d4a582c5907d34d1b4a55055772bd7
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-04-24 20:35:29 -0700

    Add custom battery options (1/2)

--- src/com/android/settings/SystemUISettings.java
+++ src/com/android/settings/SystemUISettings.java
@@ -15,18 +15,30 @@ import android.provider.Settings;
 import android.text.Spannable;
 import android.widget.EditText;
 
+import com.android.settings.util.colorpicker.ColorPickerPreference;
+
 public class SystemUISettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String PREF_CARRIER_TEXT = "carrier_text";
     private static final String PREF_IME_SWITCHER = "ime_switcher";
     private static final String PREF_CLOCK_DISPLAY_STYLE = "clock_am_pm";
     private static final String PREF_CLOCK_STYLE = "clock_style";
+    private static final String BATTERY_TEXT = "battery_text";
+    private static final String BATTERY_STYLE = "battery_style";
+    private static final String BATTERY_BAR = "battery_bar";
+    private static final String BATTERY_BAR_COLOR = "battery_bar_color";
+    private static final String BATTERY_TEXT_COLOR = "battery_text_color";
     private Preference mCarrier;
     private CheckBoxPreference mShowImeSwitcher;
     private ListPreference mAmPmStyle;
     private ListPreference mClockStyle;
+    private CheckBoxPreference mBattText;
+    private CheckBoxPreference mBattBar;
+    private ColorPickerPreference mBattBarColor;
+    private ListPreference mBatteryStyle;
 
     String mCarrierText = null;
+    PreferenceScreen mBattColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,23 @@ public class SystemUISettings extends SettingsPreferenceFragment implements OnPr
         mClockStyle = (ListPreference) prefSet.findPreference(PREF_CLOCK_STYLE);
         mAmPmStyle = (ListPreference) prefSet.findPreference(PREF_CLOCK_DISPLAY_STYLE);
 
+        mBattText = (CheckBoxPreference) prefSet.findPreference(BATTERY_TEXT);
+        mBattText.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.BATTERY_TEXT, 0) == 1);
+
+        mBattBar = (CheckBoxPreference) prefSet.findPreference(BATTERY_BAR);
+        mBattBar.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUSBAR_BATTERY_BAR, 0) == 1);
+
+        mBattColor = (PreferenceScreen) findPreference(BATTERY_TEXT_COLOR);
+        mBattColor.setEnabled(mBattText.isChecked());
+
+        mBattBarColor = (ColorPickerPreference) prefSet.findPreference(BATTERY_BAR_COLOR);
+        mBattBarColor.setOnPreferenceChangeListener(this);
+        mBattBarColor.setEnabled(mBattBar.isChecked());
+
+	mBatteryStyle = (ListPreference) prefSet.findPreference(BATTERY_STYLE);
+
         int styleValue = Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_AM_PM, 2);
         mAmPmStyle.setValueIndex(styleValue);
@@ -46,6 +75,11 @@ public class SystemUISettings extends SettingsPreferenceFragment implements OnPr
                 Settings.System.STATUS_BAR_CLOCK, 1);
         mClockStyle.setValueIndex(clockVal);
         mClockStyle.setOnPreferenceChangeListener(this);
+
+        int battVal = Settings.System.getInt(getContentResolver(),
+                Settings.System.BATTERY_PERCENTAGES, 1);
+        mBatteryStyle.setValueIndex(battVal);
+        mBatteryStyle.setOnPreferenceChangeListener(this);
 
 	mShowImeSwitcher = (CheckBoxPreference) findPreference(PREF_IME_SWITCHER);
 	mShowImeSwitcher.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
@@ -64,6 +98,20 @@ public class SystemUISettings extends SettingsPreferenceFragment implements OnPr
         }
     }
 
+    private void updateBatteryTextToggle(boolean bool) {
+        if (bool)
+            mBattColor.setEnabled(true);
+        else
+            mBattColor.setEnabled(false);
+    }
+
+    private void updateBatteryBarToggle(boolean bool) {
+        if (bool)
+            mBattBarColor.setEnabled(true);
+        else
+            mBattBarColor.setEnabled(false);
+    }
+
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
         if (preference == mShowImeSwitcher) {
@@ -71,6 +119,18 @@ public class SystemUISettings extends SettingsPreferenceFragment implements OnPr
 	    Settings.System.putInt(getActivity().getContentResolver(),
 		Settings.System.SHOW_STATUSBAR_IME_SWITCHER, checked ? 1 : 0);
 	    return true;
+        } else if (preference == mBattText) {
+            value = mBattText.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.BATTERY_TEXT, value ? 1 : 0);
+            updateBatteryTextToggle(value);
+            return true;
+        } else if (preference == mBattBar) {
+            value = mBattBar.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUSBAR_BATTERY_BAR, value ? 1 : 0);
+            updateBatteryBarToggle(value);
+            return true;
         } else if (preference == mCarrier) {
             AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());
             ad.setTitle("Custom Carrier Text");
@@ -104,6 +164,19 @@ public class SystemUISettings extends SettingsPreferenceFragment implements OnPr
             int val = Integer.valueOf((String) newValue);
             Settings.System.putInt(getContentResolver(),
                 Settings.System.STATUS_BAR_CLOCK, val);
+            return true;
+        } else if (preference == mBatteryStyle) {
+            int val = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.BATTERY_PERCENTAGES, val);
+            return true;
+        } else if (preference == mBattBarColor) {
+            String hexColor = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(newValue)));
+            preference.setSummary(hexColor);
+            int color = ColorPickerPreference.convertToColorInt(hexColor);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUSBAR_BATTERY_BAR_COLOR, color);
             return true;
         }
         return false;
