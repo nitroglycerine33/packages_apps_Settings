c5741aeee0da3daeacd88c843eab56896c3d324f
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-04-26 13:41:34 -0700

    Added color picker for toggle widget indicators

--- src/com/android/settings/cyanogenmod/PowerWidget.java
+++ src/com/android/settings/cyanogenmod/PowerWidget.java
@@ -45,6 +45,8 @@ import android.widget.TextView;
 import com.android.internal.telephony.Phone;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.cyanogenmod.TouchInterceptor;
+import com.android.settings.util.colorpicker.ColorPickerPreference;
 
 public class PowerWidget extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -63,6 +65,8 @@ public class PowerWidget extends SettingsPreferenceFragment implements
     private static final String UI_EXP_WIDGET_PICKER = "widget_picker";
 
     private static final String UI_EXP_WIDGET_ORDER = "widget_order";
+    
+    private static final String UI_WIDGET_COLOR = "widget_color";
 
     private CheckBoxPreference mPowerWidget;
 
@@ -77,6 +81,8 @@ public class PowerWidget extends SettingsPreferenceFragment implements
     private PreferenceScreen mPowerPicker;
 
     private PreferenceScreen mPowerOrder;
+    
+    private ColorPickerPreference mWidgetColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -97,6 +103,9 @@ public class PowerWidget extends SettingsPreferenceFragment implements
             mPowerWidgetHapticFeedback = (ListPreference) prefSet
                     .findPreference(UI_EXP_WIDGET_HAPTIC_FEEDBACK);
             mPowerWidgetHapticFeedback.setOnPreferenceChangeListener(this);
+            mWidgetColor = (ColorPickerPreference) prefSet
+            		.findPreference(UI_WIDGET_COLOR);
+            mWidgetColor.setOnPreferenceChangeListener(this);
 
             mPowerPicker = (PreferenceScreen) prefSet.findPreference(UI_EXP_WIDGET_PICKER);
             mPowerOrder = (PreferenceScreen) prefSet.findPreference(UI_EXP_WIDGET_ORDER);
@@ -124,6 +133,14 @@ public class PowerWidget extends SettingsPreferenceFragment implements
             int intValue = Integer.parseInt((String) newValue);
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.EXPANDED_HAPTIC_FEEDBACK, intValue);
+            return true;
+        } else if (preference == mWidgetColor) {
+        	String hexColor = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(newValue)));
+            preference.setSummary(hexColor);
+            int color = ColorPickerPreference.convertToColorInt(hexColor);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.EXPANDED_VIEW_WIDGET_COLOR, color);
             return true;
         }
         return false;
