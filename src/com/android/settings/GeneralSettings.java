067b67b4f8da72479db7b0a0ed98aec56677d990
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-08-09 08:47:13 -0500

    Clean up Eclipse Settings layout

--- src/com/android/settings/GeneralSettings.java
+++ src/com/android/settings/GeneralSettings.java
@@ -1,72 +1,30 @@
 package com.android.settings;
 
-import android.app.AlertDialog;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
-import android.provider.Settings;
-import android.text.Spannable;
-import android.widget.EditText;
 
 import java.util.List;
 
 public class GeneralSettings extends SettingsPreferenceFragment {
 
-private static final String PREF_CARRIER_TEXT = "carrier_text";
+    private static final String QUIET_HOURS = "quiet_hours";
+    private static final String WEATHER_SETTINGS = "weather_settings";
 
-private Preference mCarrier;
+    PreferenceScreen mQuietHours;
+    PreferenceScreen mWeatherSettings;
 
-String mCarrierText = null;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.general_settings);
+
         PreferenceScreen prefs = getPreferenceScreen();
 
-	mCarrier = (Preference) prefs.findPreference(PREF_CARRIER_TEXT);
-	updateCarrierText();
-    }
+        mQuietHours = (PreferenceScreen) findPreference(QUIET_HOURS);
+        mWeatherSettings = (PreferenceScreen) findPreference(WEATHER_SETTINGS);
 
-    private void updateCarrierText() {
-        mCarrierText = Settings.System.getString(getContentResolver(), Settings.System.CUSTOM_CARRIER_LABEL);
-        if (mCarrierText == null) {
-            mCarrier.setSummary("Upon changing you will need to wipe data to return to stock. Requires reboot.");
-        } else {
-            mCarrier.setSummary(mCarrierText);
-        }
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-	boolean value;
-
-        if (preference == mCarrier) {
-            AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());
-            ad.setTitle("Custom Carrier Text");
-            ad.setMessage("Enter new carrier text here");
-            final EditText text = new EditText(getActivity());
-            text.setText(mCarrierText != null ? mCarrierText : "");
-            ad.setView(text);
-            ad.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    String value = ((Spannable) text.getText()).toString();
-                    Settings.System.putString(getActivity().getContentResolver(), Settings.System.CUSTOM_CARRIER_LABEL, value);
-                    updateCarrierText();
-                }
-            });
-            ad.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                }
-            });
-            ad.show();
-        }
-
-        return true;
     }
 }
