f69f57ba47801a374a5e9e7494baa8e9f614d453
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-08-16 09:31:58 -0500

    Add Volume Panel Settings (Split ringtone and notification volume)

---
+++ src/com/android/settings/GeneralUI.java
@@ -0,0 +1,53 @@
+package com.android.settings;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.text.Spannable;
+import android.widget.EditText;
+
+public class GeneralUI extends SettingsPreferenceFragment {
+
+    public static final String TAG = "GeneralUI";
+
+    private static final String PREF_ENABLE_VOLUME_OPTIONS = "enable_volume_options";
+
+    CheckBoxPreference mEnableVolumeOptions;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.general_ui);
+
+        mEnableVolumeOptions = (CheckBoxPreference) findPreference(PREF_ENABLE_VOLUME_OPTIONS);
+        mEnableVolumeOptions.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.ENABLE_VOLUME_OPTIONS, 0) == 1);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+         if (preference == mEnableVolumeOptions) {
+
+            boolean checked = ((CheckBoxPreference) preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.ENABLE_VOLUME_OPTIONS, checked ? 1 : 0);
+            return true;
+            }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}
