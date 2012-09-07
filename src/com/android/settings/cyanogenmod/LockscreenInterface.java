70af10bf6f6fddee631e9fa8eea856cf20be5157
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-09-06 21:45:26 -0500

    Add lockscreen weather and calendar (2/2) - cleanup settings in another commit

---
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -0,0 +1,112 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import java.io.File;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.view.Display;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class LockscreenInterface extends SettingsPreferenceFragment {
+    private static final String TAG = "LockscreenInterface";
+    public static final String KEY_WEATHER_PREF = "lockscreen_weather";
+    public static final String KEY_CALENDAR_PREF = "lockscreen_calendar";
+
+    private Preference mWeatherPref;
+    private Preference mCalendarPref;
+    private Activity mActivity;
+    ContentResolver mResolver;
+
+    private boolean mIsScreenLarge;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = getActivity();
+        mResolver = mActivity.getContentResolver();
+
+        addPreferencesFromResource(R.xml.lockscreen_interface_settings);
+        mWeatherPref = (Preference) findPreference(KEY_WEATHER_PREF);
+        mCalendarPref = (Preference) findPreference(KEY_CALENDAR_PREF);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateState();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void updateState() {
+        int resId;
+
+        // Set the weather description text
+        if (mWeatherPref != null) {
+            boolean weatherEnabled = Settings.System.getInt(mResolver,
+                    Settings.System.LOCKSCREEN_WEATHER, 0) == 1;
+            if (weatherEnabled) {
+                mWeatherPref.setSummary(R.string.lockscreen_weather_enabled);
+            } else {
+                mWeatherPref.setSummary(R.string.lockscreen_weather_summary);
+            }
+        }
+
+        // Set the calendar description text
+        if (mCalendarPref != null) {
+            boolean weatherEnabled = Settings.System.getInt(mResolver,
+                    Settings.System.LOCKSCREEN_CALENDAR, 0) == 1;
+            if (weatherEnabled) {
+                mCalendarPref.setSummary(R.string.lockscreen_calendar_enabled);
+            } else {
+                mCalendarPref.setSummary(R.string.lockscreen_calendar_summary);
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+}
